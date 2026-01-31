19f571c2a65066b6df441d7fb27175b1b4becb30
Varsha <varsh6362@gmail.com>
2026-01-31 12:27:42 +0530

    hello

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
+
+@app.route("/abc")
+def abc():
+    return render_template("abc.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
